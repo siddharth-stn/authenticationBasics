8a7ebd4f8a068fc8f13eb235cd25f888c7397998
siddharth-stn <sidarjunstn.sp@gmail.com>
2022-12-28 11:58:10 +0530

    Bug in app.use(LocalStrategy...)

--- app.js
+++ app.js
@@ -9,14 +9,15 @@ const Schema = mongoose.Schema;
 const mongoDb =
   "mongodb+srv://m001-student:mongodb-basics@sandbox.ed9rkru.mongodb.net/authenticationBasics?retryWrites=true&w=majority";
 
+mongoose.set("strictQuery", false);
+
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlparser: true });
+
 const db = mongoose.connection;
 
 db.on("error", () => {
   console.error("mongo connection error");
 });
-
-mongoose.set("strictQuery", false);
 
 const User = mongoose.model(
   "User",
@@ -31,19 +32,63 @@ app.set("views", __dirname);
 app.set("view engine", "pug");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+app.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res, next) => res.render("index"));
+app.get("/", (req, res, next) => res.render("index", { user: req.user }));
 app.get("/sign-up", (req, res, next) => res.render("sign-up-form"));
 app.post("/sign-up", (req, res, next) => {
   const user = new User({
     username: req.body.username,
     password: req.body.password,
   });
-
   user.save((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
     if (err) {
       return next(err);
     }
